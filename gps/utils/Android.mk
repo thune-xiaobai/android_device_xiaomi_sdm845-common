c39632decad8f97b0486c145876e6836aa73e8cf
xiaobai <fengz181x@gmail.com>
2018-12-03 15:00:16 +0800

    sdm845-common: update gps from caf
    * Tag:LA.UM.7.3.r1-06300-sdm845.0
    
    Change-Id: Ia0696df780569c54c1936a1e9748e5b9be4bf0d3

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,13 +1,15 @@
+#Compile this library only for builds with the latest modem image
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 
 ## Libs
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
-    liblog \
-    libloc_pla
+    liblog
 
 LOCAL_SRC_FILES += \
     loc_log.cpp \
@@ -15,7 +17,6 @@ LOCAL_SRC_FILES += \
     msg_q.c \
     linked_list.c \
     loc_target.cpp \
-    platform_lib_abstractions/elapsed_millis_since_boot.cpp \
     LocHeap.cpp \
     LocTimer.cpp \
     LocThread.cpp \
@@ -37,13 +38,15 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
 LOCAL_HEADER_LIBRARIES := \
+    libutils_headers \
     libloc_pla_headers \
     liblocation_api_headers
 
 LOCAL_MODULE := libgps.utils
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_PRELINK_MODULE := false
 
 LOCAL_CFLAGS += $(GNSS_CFLAGS)
 
@@ -53,5 +56,3 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libgps.utils_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 include $(BUILD_HEADER_LIBRARY)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
